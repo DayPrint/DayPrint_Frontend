7dc1be761379b34c76d476e296822cae1acc8fe0
Kim-Eun-Bin <kiobn1201@gmail.com>
2022-05-09 16:12:51 +0900

    AddPostPage map 함수 수정

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -9,8 +9,7 @@ const MainPage = () => {
         <div className="account">OOO님의 기념일을 친구와 공유해보세요!</div>
       </div>
       <div className="MyWorkplace">
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
+        <div><a href="/add">my story</a></div>
         <div><a href="/post">my story</a></div>
         <div><a href="/post">my story</a></div>
         <div><a href="/post">my story</a></div>
--- src/component/index.js
+++ src/component/index.js
@@ -1,8 +1,8 @@
-import MainPage from "./MainPage";
-import Comment from "./Comment";
-import AddPostPage from "./AddPostPage";
-import LoginPage from "./LoginPage";
-import PostPage from "./PostPage";
-import Header from "./Header";
+import MainPage from './MainPage';
+import Comment from './Comment';
+import AddPostPage from './AddPostPage';
+import LoginPage from './LoginPage';
+import PostPage from './PostPage';
+import Header from './Header';
 
-export {MainPage,Comment,AddPostPage,LoginPage,PostPage,Header};
+export { MainPage, Comment, AddPostPage, LoginPage, PostPage, Header };
