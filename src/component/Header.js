c3d1d5486b23d7f55dcee9fe3cf71e05d3bd1664
jaeyongish <jaeyongish@gmail.com>
2022-05-10 15:00:54 +0900

    header부분 작업중

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import menu_icon from "../style/images/bars-solid.svg";
+import footprint_icon from "../style/images/shoe-prints-solid.svg";
 import user_icon from "../style/images/user-solid.svg";
 
 const Header = () => {
   return (
     <div id="header" className="container cf">
       <ul>
-        <li><a href="#"><img className="icon" src={menu_icon} alt="menu"/></a></li>
+        <li><a href="#">스토리 작성하기 <img className="icon" src={footprint_icon} alt="menu"/></a></li>
         <li><a href="/" className="home_btn">DayPrint</a></li>
-        <li><a href="/login"><img className="icon" src={user_icon} alt="user"/></a></li>
+        <li><a href="/login">로그인 <img className="icon" src={user_icon} alt="user"/></a></li>
       </ul>
     </div>
   )
