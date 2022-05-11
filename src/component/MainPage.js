570ba18348bdada8e09cbcbbc9f60cfd6303e8bc
jaeyongish <jaeyongish@gmail.com>
2022-05-11 16:06:17 +0900

    캐러셀 제거

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -20,24 +20,9 @@ const MainPage = () => {
         <div><a href="/post">yyyy.mm.dd</a></div>
         <div><a href="/post">yyyy.mm.dd</a></div>
         <div><a href="/post">yyyy.mm.dd</a></div>
-        
       </div>
       <div className="Instagram">instagram</div>
-      <div className="carousel">
-        <a className="carousel-item" href="#one!"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/409269/food-drink-08.jpg" /></a>
-        <a className="carousel-item" href="#two!"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/409269/food-drink-09.jpg" /></a>
-        <a className="carousel-item" href="#three!"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/409269/food-drink-10.jpg" /></a>
-        <a className="carousel-item" href="#four!"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/409269/food-drink-11.jpg" /></a>
-        <a className="carousel-item" href="#five!"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/409269/food-drink-12.jpg" /></a>
-      </div>
-      <div className="controls">
-        <span className="prev btn-floating">
-          <i className="material-icons">arrow_back</i>
-        </span>
-        <span className="next btn-floating">
-          <i className="material-icons">arrow_forward</i>
-        </span>
-      </div>
+      
     </div>
   )
 }
