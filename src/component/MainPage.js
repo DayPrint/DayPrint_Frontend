90aae1c972e623c955de6015b47539420e4b16c4
jaeyongish <jaeyongish@gmail.com>
2022-05-11 18:04:49 +0900

    rollback

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -12,17 +12,15 @@ const MainPage = () => {
         <div className="account">OOO님의 기념일을 친구와 공유해보세요!</div>
       </div>
       <div className="MyWorkplace">
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
-        <div><a href="/post">my story</a></div>
+        <div><a href="/post">2022.05.10</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
+        <div><a href="/post">yyyy.mm.dd</a></div>
       </div>
       {/* <div className="Instagram">instagram</div> */}
       <Footer />
--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -6,29 +6,38 @@ const LoginPage = () => {
 
   return (
     <div id="sign_up">
-      <Header />
+      {/* <Header /> */}
       <div className="content">
-        <h2>무료로 계정을 만들어보세요!</h2>
-        <form autoComplete="off" action="" method="POST">
+        <h2 class="banner-tt"><a href="/">DayPrint </a>회원가입</h2>
+        <form autoComplete="off" action="" method="POST" class="sign_up_form">
           <div className="field">
+            <label for="userName" class="hidden">Name</label>
             <i className="fas fa-user" />
-            <input type="text" id="userName" name="username" placeholder="이름" />
+            <input type="text" id="userName" name="username" placeholder="  아이디" />
           </div>
           <div className="field">
+            <label for="userEmail" class="hidden">email</label>
             <i className="far fa-envelope" />
-            <input type="email" id="userEmail" name="useremail" placeholder="이메일 주소" />
+            <input type="email" id="userEmail" name="useremail" placeholder="  이메일 주소" />
           </div>
           <div className="field">
+            <label for="userPass" class="hidden">password</label>
             <i className="fas fa-lock" />
-            <input type="password" id="userPass" name="userpw" placeholder="비밀번호" />
+            <input type="password" id="userPass" name="userpw" placeholder="  비밀번호" />
           </div>
           <div className="field">
+            <label for="userPass" class="hidden">password</label>
             <i className="fas fa-lock" />
-            <input type="password" id="passConfirm" placeholder="비밀번호 확인" />
+            <input type="password" id="passConfirm" placeholder="  비밀번호 확인" />
           </div>
-          <input type="submit" className="signup_btn" value="DayPrint 계정 만들기" />
+          <input type="submit" className="field" value="DayPrint 계정 만들기" />
         </form>
-        <a href="/login" className="login_btn">이미 회원이세요? 로그인하기</a>
+        <div class="sign_up_container">
+          <div class="sign_up_link">
+            <a href="/" class="link">홈으로</a>
+            <a href="/login" class="link">이미 회원이세요? 로그인하기</a>
+          </div>
+        </div>
       </div>
     </div>
   )
