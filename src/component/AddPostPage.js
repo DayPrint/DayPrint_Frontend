d45ff709bad1c61167451370a139dbe67d805d9b
SeoSeongHyeop <tjtjdguq77@naver.com>
2022-05-11 09:48:25 +0900

    addpostpage modified

--- src/component/AddPostPage.js
+++ src/component/AddPostPage.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/button.css';
 import '../style/addpostpage.css';
 
 const AddPostPage = () => {
+    const theme=['Christmas', 'New Year', 'Birthday', 'Graduation', 'Marriage'];
+    const Theme={
+        "Christmas":"Christmas",
+        "NewYear":"New Year",
+        "Birthday":"Birthday",
+        "Graduation":"Graduation",
+        "Marriage":"Marriage"
+    }
+    const [thema,setTheme]=useState(Theme.Christmas);
+    const changeTheme=(params,e)=>{
+        //this.thema=Theme.e.target.value;
+        const {value,name}=e.target;
+        console.log(value,name);
+        console.log(params);
+    }
+    // this.setState = {
+    //     selectList: ['Christmas', 'New Year', 'Birthday', 'Graduation', 'Marriage'],
+    //     selectValue: "Christmas"
+    // };
 
-  const theme = ['Christmas', 'New Year', 'Birthday', 'Graduation', 'Marriage'];
-  return (
-    <div>
-      <div>
-        <ul id="theme">
-          {theme.map((th, i) => {
-            return <li>{th}</li>
-          })}
-        </ul>
-      </div>
-      <div>
-        <form>
-          <label for="date">기념일 날짜 : </label>
-          <input type="datetime-local" id="date"></input>
-          <label for="theme">테마 : </label>
-          {theme.map((th, i) => {
-            <input type="radio" id="theme" checked>th</input>
-          })}
-        </form>
-      </div>
-    </div>
-  );
+    // const onChange = (e) => {
+    //     this.setState({ selectValue: e.target.value });
+    // }
+    return (
+        <div>
+            <div>
+                <ul id="theme">
+                    {theme.map((th, i) => {
+                        return <li onClick={changeTheme} value={th}>{th}</li>
+                    })}
+                </ul>
+            </div>
+            <div>
+            
+                <form>
+                    <label for="date">기념일 날짜 : </label>
+                    <input type="datetime-local" id="date"></input>
+                    <p>테마 : </p>
+                    {/* {theme.map((th, i) => {
+                        <React.Fragment key={i}>
+                            <input id={th} value={th} name="theme" type="radio" checked={this.state.selectValue == th} onChange={this.onChange} />
+                        </React.Fragment>
+                    })} */}
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default AddPostPage;
