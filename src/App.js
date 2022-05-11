b246a69ac30ecae5bec8688ef5a3a4fff73e1cec
SeoSeongHyeop <tjtjdguq77@naver.com>
2022-05-11 16:49:46 +0900

    addpostpage form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { MainPage, LoginPage, PostPage, Comment, AddPostPage,Header } from './component';
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './component/SignUp';
+import CommentStyle from './component/CommentStyle';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/post" element={<PostPage />} />
+        <Route path="post/decor" element={<CommentStyle />} />
         <Route path="/post/comment" element={<Comment />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/add" element={<AddPostPage />} />
--- src/component/AddPostPage.js
+++ src/component/AddPostPage.js
@@ -1,53 +1,56 @@
 import React, { useState } from 'react';
 import '../style/css/button.css';
 import '../style/css/addpostpage.css';
+import img from '../style/images/file-image-solid.svg'
+import '../style/css/button.css'
 
 const AddPostPage = () => {
-    const theme=['Christmas', 'New Year', 'Birthday', 'Graduation', 'Marriage'];
-    const Theme={
-        "Christmas":"Christmas",
-        "NewYear":"New Year",
-        "Birthday":"Birthday",
-        "Graduation":"Graduation",
-        "Marriage":"Marriage"
-    }
-    const [thema,setTheme]=useState(Theme.Christmas);
-    const changeTheme=(params,e)=>{
-        //this.thema=Theme.e.target.value;
-        const {value,name}=e.target;
-        console.log(value,name);
-        console.log(params);
-    }
-    // this.setState = {
-    //     selectList: ['Christmas', 'New Year', 'Birthday', 'Graduation', 'Marriage'],
-    //     selectValue: "Christmas"
-    // };
-
-    // const onChange = (e) => {
-    //     this.setState({ selectValue: e.target.value });
+    // const theme = ['Christmas', 'New Year', 'Birthday', 'Graduation', 'Marriage'];
+    // const Theme = {
+    //     "Christmas": "Christmas",
+    //     "NewYear": "New Year",
+    //     "Birthday": "Birthday",
+    //     "Graduation": "Graduation",
+    //     "Marriage": "Marriage"
+    // }
+    // const [thema, setTheme] = useState(Theme.Christmas);
+    // const changeTheme = (params, e) => {
+    //     //this.thema=Theme.e.target.value;
+    //     const { value, name } = e.target;
+    //     console.log(value, name);
+    //     console.log(params);
     // }
     return (
-        <div>
-            <div>
+        <div className='addpostform'>
+            {/* <div>
                 <ul id="theme">
                     {theme.map((th, i) => {
                         return <li onClick={changeTheme} value={th}>{th}</li>
                     })}
                 </ul>
-            </div>
-            <div>
-            
-                <form>
-                    <label for="date">기념일 날짜 : </label>
-                    <input type="datetime-local" id="date"></input>
-                    <p>테마 : </p>
-                    {/* {theme.map((th, i) => {
-                        <React.Fragment key={i}>
-                            <input id={th} value={th} name="theme" type="radio" checked={this.state.selectValue == th} onChange={this.onChange} />
-                        </React.Fragment>
-                    })} */}
-                </form>
-            </div>
+            </div> */}
+            <form autoComplete="off" action="" method="POST">
+                <div class="filebox">
+                    <label for="file"><img src={img} /></label>
+                    <input class="upload-name" value="첨부파일" placeholder="첨부파일" />
+                    <input type="file" id="file" />
+                </div>
+                <div class='form'>
+                    <div>
+                        <label for="title">나의 기념일</label>
+                        <input type="text" id='title' name="title" placeholder="제목" value="제목" />
+                    </div>
+                    <div>
+                        <label for="date">기념 날짜</label>
+                        <input type="datetime-local" id='date' name="date" placeholder="날짜" />
+                    </div>
+                    <div>
+                        <label for="explanation">설명</label>
+                        <input type="postContent" id='explanation' name="explanation" placeholder='설명' />
+                    </div>
+                </div>
+            </form>
+            <a href="/post" className="myButton">입력</a>
         </div>
     );
 };
