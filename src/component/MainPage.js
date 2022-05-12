93406d3437180df287b038fbff894d53c9659cce
Seo Seong Hyeop <45827834+tjtjdguq@users.noreply.github.com>
2022-05-12 10:23:26 +0900

    Update MainPage.js

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../style/css/styles.css';
 import Footer from './Footer';
+import Header from './Header.js'
 
 
 const MainPage = () => {
 
   return (
+    <Header/>
     <div>
       <div className="MyAccount">
         <div className="account">OOO님의 기념일을 친구와 공유해보세요!</div>
