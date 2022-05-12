ba64141517847ef019df3ae83cb1c7793db7a786
SeoSeongHyeop <tjtjdguq77@naver.com>
2022-05-12 17:52:17 +0900

    signup page item update

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container cf">
         <nav>
             <ul className="Header_bar cf">
-                <li className="Header_add"><a href="#" alt="add_story">스토리 작성하기 <i class="fa-solid fa-shoe-prints"></i></a></li>
+                <li className="Header_add"><a href="add" alt="add_story">스토리 작성하기 <i class="fa-solid fa-shoe-prints"></i></a></li>
                 <li className="Header_logo"><a href="/" alt="home">DayPrint</a></li>
                 <li className="Header_account"><a href="/login" alt="login">로그인 <i class="fa-solid fa-user"></i></a></li>
             </ul>
--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import '../style/css/imageupload.css';
+import img from '../style/images/file-image-solid.svg'
 import '../style/styles.css';
 const LoginPage = () => {
+  const [inputs, setInputs]=useState({
+    username:"",
+    phone:"",
+    imagefile:"",
+    id:"",
+    passwd1:"",
+    passwd2:""
 
+  })
+  const {username,phone,imagefile,id,passwd1,passwd2}=inputs;
+  const onChange=e=>{
+    setInputs({
+      ...inputs,
+      [e.target.name]:e.target.value
+    });
+  };
   return (
     <div id="sign_up">
 
@@ -12,23 +29,33 @@ const LoginPage = () => {
           <div className="field">
             <label for="userName" class="hidden">Name</label>
             <i className="fas fa-user" />
-            <input type="text" id="userName" name="username" placeholder="  아이디" />
+            <input type="text" id="userName" name="username" placeholder="  사용자이름" />
+          </div>
+          <div className="field">
+            <label for="phone" class="hidden">PhoneNumber</label>
+            <i className="fas fa-user" />
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}" placeholder="  연락처" />
           </div>
           <div className="field">
             <label for="userEmail" class="hidden">email</label>
             <i className="far fa-envelope" />
-            <input type="email" id="userEmail" name="useremail" placeholder="  이메일 주소" />
+            <input type="email" id="userEmail" name="useremail" placeholder="  아이디(E-mail)" required />
           </div>
           <div className="field">
             <label for="userPass" class="hidden">password</label>
             <i className="fas fa-lock" />
-            <input type="password" id="userPass" name="userpw" placeholder="  비밀번호" />
+            <input type="password" id="userPass" name="userpw" placeholder="  비밀번호" required/>
           </div>
           <div className="field">
             <label for="userPass" class="hidden">password</label>
             <i className="fas fa-lock" />
             <input type="password" id="passConfirm" placeholder="  비밀번호 확인" />
           </div>
+          <div class="filebox">
+                    <label for="file"><img src={img} /></label>
+                    <input class="upload-name" value="첨부파일" placeholder="첨부파일" />
+                    <input type="file" id="file" />
+                </div>
           <input type="submit" className="field" value="DayPrint 계정 만들기" />
         </form>
         <div class="sign_up_container">
