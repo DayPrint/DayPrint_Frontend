c2e577f14af181f4457651ca77d97a430c901fa8
SeoSeongHyeop <tjtjdguq77@naver.com>
2022-05-12 18:09:21 +0900

    signup status added

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -18,6 +18,7 @@ const LoginPage = () => {
       ...inputs,
       [e.target.name]:e.target.value
     });
+    console.log(inputs);
   };
   return (
     <div id="sign_up">
@@ -29,32 +30,32 @@ const LoginPage = () => {
           <div className="field">
             <label for="userName" class="hidden">Name</label>
             <i className="fas fa-user" />
-            <input type="text" id="userName" name="username" placeholder="  사용자이름" />
+            <input type="text" id="userName" name="username" onChange={onChange} placeholder="  사용자이름" />
           </div>
           <div className="field">
             <label for="phone" class="hidden">PhoneNumber</label>
             <i className="fas fa-user" />
-            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}" placeholder="  연락처" />
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}" onChange={onChange} placeholder="  연락처" />
           </div>
           <div className="field">
             <label for="userEmail" class="hidden">email</label>
             <i className="far fa-envelope" />
-            <input type="email" id="userEmail" name="useremail" placeholder="  아이디(E-mail)" required />
+            <input type="email" id="userEmail" name="id" onChange={onChange} placeholder="  아이디(E-mail)" required />
           </div>
           <div className="field">
             <label for="userPass" class="hidden">password</label>
             <i className="fas fa-lock" />
-            <input type="password" id="userPass" name="userpw" placeholder="  비밀번호" required/>
+            <input type="password" id="userPass" name="passwd1" onChange={onChange} placeholder="  비밀번호" required/>
           </div>
           <div className="field">
             <label for="userPass" class="hidden">password</label>
             <i className="fas fa-lock" />
-            <input type="password" id="passConfirm" placeholder="  비밀번호 확인" />
+            <input type="password" id="passConfirm" name="passwd2" onChange={onChange} placeholder="  비밀번호 확인" />
           </div>
           <div class="filebox">
                     <label for="file"><img src={img} /></label>
                     <input class="upload-name" value="첨부파일" placeholder="첨부파일" />
-                    <input type="file" id="file" />
+                    <input type="file" id="file" name="imagefile" onChange={onChange} />
                 </div>
           <input type="submit" className="field" value="DayPrint 계정 만들기" />
         </form>
