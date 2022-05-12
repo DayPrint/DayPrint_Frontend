23b6124398452d2eb6745ce5dd38345878026486
SeoSeongHyeop <tjtjdguq77@naver.com>
2022-05-12 10:00:02 +0900

    addpostpage merge

---
+++ src/component/CommentStyle.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../style/css/CommentStyle.css';
+
+
+const CommentStyle =() =>{
+    return(
+        <div id="decor">
+            <h1>장식을 선택하세요</h1>
+            <ul>
+                <li>decor1</li>
+            </ul>
+        </div>
+    )
+}
+export default CommentStyle;
