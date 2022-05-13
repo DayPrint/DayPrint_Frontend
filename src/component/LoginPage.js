662f97e17665713b60c93140008cde9d4e7e48d1
burigori <jamasa97@icloud.com>
2022-05-13 15:08:32 +0900

    login page 구현

--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -1,25 +1,69 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import '../style/styles.css';
 
 const LoginPage = () => {
+
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: ""
+  })
+  const [results, setResults] = useState({
+    id: "",
+    Autorization: ""
+  })
+
+  const onChange = e => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value
+    });
+    console.log(inputs);
+  };
+
+  const getLoginData = (event) => {
+    event.preventDefault();
+    console.log(inputs.email);
+    console.log(inputs.password);
+    try {
+      console.log("login 실행")
+      axios.post(
+        "/login",
+        {
+        "email": inputs.email,
+        "password": inputs.password
+      }
+      ).then((res) => {
+        console.log(res);
+        setResults({
+          id: res.data.result.id,
+          Autorization: res.data.result.jwtToken
+        });
+        localStorage.setItem("user",JSON.stringify(results));
+      })
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  }
 
   return (
     <div id="login">
       {/* <Header /> */}
       <div class="content">
         <h2 class="banner-tt"><a href="/">DayPrint</a> 로그인</h2>
-        <form autoComplete="off" class="login_form">
+        <form autoComplete="off" class="login_form" onSubmit={getLoginData}>
           <div className="field">
             <label for="userName" class="hidden">Name</label>
             <i className="fas fa-user" />
-            <input type="text" id="userName" name="username" placeholder="  아이디  or  이메일" />
+            <input type="text" name="email" placeholder="  아이디  or  이메일" onChange={onChange} />
           </div>
           <div className="field">
             <label for="userPass" class="hidden">password</label>
             <i className="fas fa-lock" />
-            <input type="password" id="userPass" name="userpw" placeholder="  비밀번호" />
+            <input type="password" name="password" placeholder="  비밀번호" onChange={onChange} />
           </div>
-          <input type="submit" className="field" value="로그인" />
+          <input type="submit" className="field" value="로그인" onChange={onChange} />
         </form>
         <div>
           <div>
--- src/component/PostPage.js
+++ src/component/PostPage.js
@@ -1,106 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/postpage.css';
 import Header from './Header';
 import plus_btn from "../style/images/plus-solid.svg";
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const PostPage = () => {
+
+    let {postId} = useParams();
+    const [list, setlist] = useState([]);
+    const loadData= ()=> {
+        try{
+            console.log("loaddata 실행")
+            axios.get("/api/comment/"+ postId ,{
+                headers:{
+                    Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJ1c2VyUGsiOiJzdHJpbmciLCJpYXQiOjE2NTI0MTcwMDYsImV4cCI6MTY1MjQxODgwNn0.tCkeJxfbZqgrGAYC-AaVAPEJ5OkGXS7IrXwqa31KqPc",
+                },
+            }
+            )
+            .then((res) => {
+                res.data.map((cur) => {
+                    setlist(prev => [...prev, cur]);
+                    console.log(cur);
+            });
+            console.log(list.map((cur)=>{
+                console.log(cur);
+            }))
+        })
+        } catch(error){
+            console.log("loadData 오류");
+            console.log(error);
+        }
+        return;
+    };
+
+    useEffect(()=>{
+        loadData();
+    }, [])
+
 
   return (
     <body>
       <Header />
       <main>
-          <div class="border1">
-              <div class="border2">
-                  <h2>22.05.19</h2>
-                  <div class="postpage_container">
+          <div className="border1">
+              <div className="border2">
+                  <h2>22.05.19({list.length})</h2>
+                  <div className="postpage_container">
                       <nav>
-                          <div class="postpoage_picture">
+                          <div className="postpoage_picture">
                               기념일 사진
                           </div>
-                          <ul class="postpage">
-                              <li class="postpage_name tape2"> {/* <!--tape는 1~5까지 종류 있음(tape1, tape2, tape3, tape4, tape5)-->*/}
+                          <ul className="postpage">
+                              <li className="postpage_name tape2"> {/* <!--tape는 1~5까지 종류 있음(tape1, tape2, tape3, tape4, tape5)-->*/}
                                   데이프린트 생일
                               </li>
-                              <li class="postpage_des">
+                              <li className="postpage_des">
                                   <p>글자는이십글자까지만글자는이십글자까지만</p>
                               </li>
                           </ul>
                       </nav>
                   </div>
-                  <div class="postpage_coms">
+                  <div className="postpage_coms">
                       <nav>
-                          <div class="com_board">
+                          <div className="com_board">
                               comment board
                           </div>
-                          <ul class="com_container">
+                          <ul className="com_container">
+                              {list.map((cur) => {
+                                <a href="">
+                                  <li>
+                                      <p className="com_bullet"><i className="fa-solid fa-thumbtack"></i>0</p>
+                                      <div className="com_text">
+                                          {cur.author}
+                                      </div>
+                                  </li>
+                                </a>
+                              })}
                               <a href="">
                                   <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
+                                      <p className="com_bullet"><i className="fa-solid fa-thumbtack"></i>0</p>
+                                      <div className="com_text">
                                           닉네임
                                       </div>
                                   </li>
                               </a>
                               <a href="">
                                   <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
-                                          닉네임
-                                      </div>
-                                  </li>
-                              </a>
-                              <a href="">
-                                  <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
-                                          닉네임
-                                      </div>
-                                  </li>
-                              </a>
-                              <a href="">
-                                  <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
-                                          닉네임
-                                      </div>
-                                  </li>
-                              </a>
-                              <a href="">
-                                  <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
-                                          닉네임
-                                      </div>
-                                  </li>
-                              </a>
-                              <a href="">
-                                  <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
-                                          닉네임
-                                      </div>
-                                  </li>
-                              </a>
-                              <a href="">
-                                  <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
-                                          닉네임
-                                      </div>
-                                  </li>
-                              </a>
-                              <a href="">
-                                  <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
-                                          닉네임
-                                      </div>
-                                  </li>
-                              </a>
-                              <a href="">
-                                  <li>
-                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
-                                      <div class="com_text">
+                                      <p className="com_bullet"><i className="fa-solid fa-thumbtack"></i>0</p>
+                                      <div className="com_text">
                                           닉네임
                                       </div>
                                   </li>
