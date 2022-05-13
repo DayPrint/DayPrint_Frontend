18661efacad84d9fed92122aed06f17177e25403
jaeyo <jaeyongish@gmail.com>
2022-05-13 09:48:52 +0900

    PostPage.js 수정, postpage.css 추가, PostPage 사진 추가

--- src/component/PostPage.js
+++ src/component/PostPage.js
@@ -1,19 +1,120 @@
 import React from 'react';
-import '../style/styles.css';
+import '../style/postpage.css';
+import Header from './Header';
 import plus_btn from "../style/images/plus-solid.svg";
-import Header from './Header.js'
 
 const PostPage = () => {
 
   return (
-    <div>
-      <Header/>
-      <div className="post_background">
-        <h1>000님에게 10개의 메세지가 왔습니다</h1>
-        <p>받은 메시지는 25일날 확인하실 수 있어요</p>
-        <a href="/post/comment"><img className="plus_btn" src={plus_btn} alt="plus"/></a>
-      </div>
-    </div>
+    <body>
+      <Header />
+      <main>
+          <div class="border1">
+              <div class="border2">
+                  <h2>22.05.19</h2>
+                  <div class="postpage_container">
+                      <nav>
+                          <div class="postpoage_picture">
+                              기념일 사진
+                          </div>
+                          <ul class="postpage">
+                              <li class="postpage_name tape2"> {/* <!--tape는 1~5까지 종류 있음(tape1, tape2, tape3, tape4, tape5)-->*/}
+                                  데이프린트 생일
+                              </li>
+                              <li class="postpage_des">
+                                  <p>글자는이십글자까지만글자는이십글자까지만</p>
+                              </li>
+                          </ul>
+                      </nav>
+                  </div>
+                  <div class="postpage_coms">
+                      <nav>
+                          <div class="com_board">
+                              comment board
+                          </div>
+                          <ul class="com_container">
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                              <a href="">
+                                  <li>
+                                      <p class="com_bullet"><i class="fa-solid fa-thumbtack"></i>0</p>
+                                      <div class="com_text">
+                                          닉네임
+                                      </div>
+                                  </li>
+                              </a>
+                          </ul>
+                      </nav>
+                  </div>
+              </div>
+          </div>
+      </main>
+      <footer>
+      </footer>
+  
+  </body>
   )
 }
 
