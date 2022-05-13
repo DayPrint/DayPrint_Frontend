71ffcbab16d5c84a27bdcad004019d19991b2b5b
jaeyo <jaeyongish@gmail.com>
2022-05-14 02:51:31 +0900

    MainPage에서 컴포넌트 분리 (MyAccount, MyWorkplace)

---
+++ src/component/MainMyWorkplace.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../style/styles.css';
+
+const MainMyWorkplace = () => {
+    return (
+        <div className="MyWorkplace">
+            <div><a href="/post">2022.05.19</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+            <div><a href="/post">yyyy.mm.dd</a></div>
+        </div>
+    )
+}
+
+export default MainMyWorkplace;
--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -2,29 +2,17 @@ import React from 'react';
 import '../style/styles.css';
 import Footer from './Footer';
 import Header from './Header.js'
+import MainMyAccount from './MainMyAccount';
+import MainMyWorkplace from './MainMyWorkplace';
 
 
 const MainPage = () => {
-
   return (
     <div>
       <Header/>
-      <div className="MyAccount">
-        <div className="account">OOO님의 기념일을 친구와 공유해보세요!</div>
-      </div>
-      <div className="MyWorkplace">
-        <div><a href="/post">2022.05.19</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-        <div><a href="/post">yyyy.mm.dd</a></div>
-      </div>
-      {/* <div className="Instagram">instagram</div> */}
-      <Footer />
+      <MainMyAccount/>
+      <MainMyWorkplace/>
+      <Footer/>
     </div>
   )
 }
