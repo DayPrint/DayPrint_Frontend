667e8bda6db26af5628edf15a41df8c1bd4a1275
SeoSeongHyeop <tjtjdguq77@naver.com>
2022-05-13 17:08:33 +0900

    postservice modified

--- src/service/post.service.js
+++ src/service/post.service.js
@@ -3,21 +3,18 @@ import axios from "axios";
 const API_URL = "http://3.36.98.223:8080/api/post/";
 
 class PostService{
-    tempUser(){
-        localStorage.setItem('user',JSON.stringify({
-            "id": 1,
-            "jwtToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJ1c2VyUGsiOiJzdHJpbmciLCJpYXQiOjE2NTI0MjY2NzcsImV4cCI6MTY1MjQyODQ3N30.UjETmD8l8dZf_H-zAOyU-U2ULI8rEJO4NLwI6uVXdIM"
-        }))
-    }
     addpost(inputs){
         const user=JSON.parse(localStorage.getItem('user'));
-        return axios.post(API_URL+user.id,{
+        const data={
             imageUrl:inputs.file,
             //theme:,
             postcontent:inputs.explanation,
             targetDate:inputs.date,
             title:inputs.title,
             userId:user.id
+        }
+        return axios.post(API_URL+user.id,data,{
+            Authorization:user.jwtToken
         }).then((res)=>console.log(res))
     }
 }
