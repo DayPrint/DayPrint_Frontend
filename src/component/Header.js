cded9e7375e953c0077bbe3511665016a405ac76
jaeyo <jaeyongish@gmail.com>
2022-05-15 02:00:53 +0900

    로그인 되어있을시로 가정하에 링크 변경, 로그아웃버튼으로 변경

--- src/component/Header.js
+++ src/component/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <ul className="Header_bar cf">
                 <li className="Header_add"><a href="add" alt="add_story">발도장 찍기 <i class="fa-solid fa-shoe-prints"></i></a></li>
                 <li className="Header_logo"><a href="/main_beforelogin" alt="home">DayPrint</a></li>
-                <li className="Header_account"><a href="/login" alt="login">로그인 <i class="fa-solid fa-user"></i></a></li>
+                <li className="Header_account"><a href="main_beforelogin" alt="login">로그아웃 <i class="fa-solid fa-user"></i></a></li>
             </ul>
         </nav>
       </div>
