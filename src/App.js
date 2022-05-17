b1c0045b1ea26f425a9917a3a8f3547cb875fe51
SeoSeongHyeop <tjtjdguq77@naver.com>
2022-05-17 11:41:53 +0900

    display postdetail

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { MainPage, LoginPage, PostPage, Comment, AddPostPage } from './component
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './component/SignUp';
 import './style/styles.css';
-import { useState } from 'react';
 
 function App() {
-  const [postData,setPostData]=useState();
   return (
     <div>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/post" element={<PostPage data={postData}/>} />
+        <Route path="/post/:id" element={<PostPage />} />
         <Route path="/post/comment" element={<Comment />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/add" element={<AddPostPage />} />
--- src/component/DisplayList.js
+++ src/component/DisplayList.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { DetailPostData } from '../Context';
 
 const DisplayPost = ({datum}) => {
     let navigator=useNavigate();
     const goToPost=()=>{
-        navigate
+        navigator("/post/"+datum.id);
     }
-    return <div key={datum.id}><a href={"http://3.36.98.223:8080/api/postdetail/"+datum.id}>{datum.title}</a></div>
-    );
+    return <div key={datum.id}><a onClick={goToPost}>{datum.title}</a></div>;
 };
 const DisplayList=({data})=>{
     return data.map((cur, index) => (<DisplayPost datum={cur} key={cur.id} />));
